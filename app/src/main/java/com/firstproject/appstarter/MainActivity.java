669505e6fa19a684a5f5da68845d353b06c292e2
Sujan Rana <khembikram1@gmail.com>
2024-06-04 10:30:57 +0545

    Linear = Lyout with button Addition

--- app/src/main/java/com/firstproject/appstarter/MainActivity.java
+++ app/src/main/java/com/firstproject/appstarter/MainActivity.java
@@ -1,6 +1,9 @@
 package com.firstproject.appstarter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btn=findViewById(R.id.confirm_button);
+        btn.setOnClickListener(new view.onClickLinstener(){
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this, "Clicked !", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
